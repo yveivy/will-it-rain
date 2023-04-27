4d359b28727ff24234d48d6e8b1671a62cce41aa
Valais Girl <Yevette@Hal-71.local>
2023-04-27 09:26:07 -0500

    added API Key to call

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6cdc1c1147e37a8245edcc10145f0367} 
 
 // **Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
 
@@ -21,7 +21,7 @@ api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 // Second API call to Geocoder converts city names and zip-codes to geo coordinates. Please note that built-in geocoder has been deprecated. Although it is still available for use, bug fixing and updates are no longer available for this functionality.
 
-https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+https://api.openweathermap.org/data/2.5/weather?q=Buda&appid=6cdc1c1147e37a8245edcc10145f0367
 
 // Required Parameters for location API
 // q	required	City name, state code (only for the US) and country code divided by comma. Please use ISO 3166 country codes.

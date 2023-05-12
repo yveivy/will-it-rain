220e50c25b5c44ff6523971906df6abc621a4e5b
Valais Girl <Yevette@Hal-71.local>
2023-05-12 09:20:11 -0500

    added event handler and API calls, grabbed elements to render current weather and append to page

--- index.js
+++ index.js
@@ -1,32 +1,114 @@
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={6cdc1c1147e37a8245edcc10145f0367} 
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-// **Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+    const cityName = document.getElementById('cityInput').value;
 
-// Required Parameters
-// lat, lon	required	Geographical coordinates (latitude, longitude). If you need the geocoder to automatic convert city names and zip-codes to geo coordinates and the other way around, please use our Geocoding API.
+    const apiKey = "6cdc1c1147e37a8245edcc10145f0367";
 
-// appid	required	Your unique API key (you can always find it on your account page under the "API key" tab)
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&limit=5&appid=${apiKey}&units=imperial`;
 
-// Optional Parameters
+    fetch(requestUrl)
+        .then(response => response.json())
+        .then(data => {
 
-// units	optional	Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more
+            console.log(data);
+            // grabbing the lat & lon for the next API call
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+            console.log(lat, lon);
+            // rounding the temps so we don't get decimals
+            const currentTemp = Math.round(data.main.temp);
+            const highTemp = Math.round(data.main.temp_max);
+            const lowTemp = Math.round(data.main.temp_min);
+            const feelsLike = Math.round(data.main.feels_like);
+            const pressure = data.main.pressure;
+            const windSpeed = data.wind.speed;
+            const humidity = data.main.humidity;
+            const location = data.name;
 
-// mode	optional	Response format. JSON format is used by default. To get data in XML format use mode=xml. Learn more
 
-// cnt	optional	A number of timestamps, which will be returned in the API response. Learn more
+            forecast(lat, lon); 
+            renderCurrentWeather(currentTemp, highTemp, lowTemp, feelsLike, pressure, windSpeed, humidity, location);
+            // City name appears on the aside under search bar
+            const displayLocation = document.getElementById("city-weather")
+            displayLocation.textContent = "Enjoy the weather in " + location;
+            console.log(location);
+   
+        })
 
-// units	optional	Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more
+});
+// second API call to get the forecast for the next week
+function forecast(lat, lon) {
+    const apiKey = "6cdc1c1147e37a8245edcc10145f0367";
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+ 
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => {
+            
+            console.log(data);
+            const daily = []
+            // iterating through the daily array to get the weather for each upcoming day
+            for (let i = 0; i < data.list.length; i+=8) {
+                const dayData = data.list[i];
+            //  rounding the temp to prevent decimals
+                const temperature = Math.round(dayData.main.temp);
+                const dateTimeString = dayData.dt_txt;
+                // Date object. The Z indicates UTC
+                const date = new Date(dateTimeString + 'Z');
+                // UTC time offset between local time with offset conversion to milliseconds
+                const timeZoneOffset = date.getTimezoneOffset() * 60 * 1000;
+                // adding offset to time date object
+                const adjustedDate = new Date(date.getTime() +timeZoneOffset);
+                // extracts day of week so we can display in our day cards with new variable dayOfWeek
+                const dayOfWeek = adjustedDate.toLocaleDateString('en-US', {weekday: 'long'});
+                console.log(`${dayOfWeek}: ${temperature}`);
+                daily.push({
+                    dayOfWeek,
+                    temperature
+                })
+            }
+           renderDailyWeather(daily)
+        })
+         
+};
+// Cool little helper function that saves time and space and you can just plug in the selector, and value 
+function setValue(selector, value, {parent = document} = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
 
-// lang	optional	You can use the lang parameter to get the output in your language. Learn more
+function renderCurrentWeather(currentTemp, highTemp, lowTemp, feelsLike, pressure, windSpeed, humidity) {
+    setValue("current-temp", currentTemp)
+    setValue("current-high", highTemp)
+    setValue("current-low", lowTemp)
+    setValue("current-fl-high", feelsLike)
+    setValue("current-pressure", pressure)
+    setValue("current-wind", windSpeed)
+    setValue("current-humidity", humidity)
+  
+}
+// grabbing the forecast section so we can display our weather for the next week so we can display the week day and temperature for that each day
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+function renderDailyWeather(daily) {
+    dailySection.innerHTML = ""
+    daily.forEach(day => {
+        const element = dayCardTemplate.content.cloneNode(true)
+        setValue("temp", day.temperature, { parent: element })
+        setValue("date", day.dayOfWeek, { parent: element})
 
-// Second API call to Geocoder converts city names and zip-codes to geo coordinates. Please note that built-in geocoder has been deprecated. Although it is still available for use, bug fixing and updates are no longer available for this functionality.
+        // tried to dynamically change the font size and text color of the daily weather but ended up removing because it didn't look great.
+        // const tempElement = element.querySelector("[data-temp]");
+        // tempElement.style.fontSize = "3rem";
+       
+        // const dateElement = element.querySelector("[data-date]");
+        // dateElement.style.fontSize = "2rem";
+        // dateElement.style.color = "navy";
+        dailySection.append(element)
+        
+    })
+}
 
-https://api.openweathermap.org/data/2.5/weather?q=Buda&appid=6cdc1c1147e37a8245edcc10145f0367
 
-// Required Parameters for location API
-// q	required	City name, state code (only for the US) and country code divided by comma. Please use ISO 3166 country codes.
 
-// appid	required	Your unique API key (you can always find it on your account page under the "API key" tab)
-
-// Daily Forecast Weather API response
 
